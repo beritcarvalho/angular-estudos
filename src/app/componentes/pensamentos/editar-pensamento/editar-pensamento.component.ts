e57ca27841e819e496d1d5d3a2eefcd9b2d7e33a
Berit Carvalho <beritcarvalho@gmail.com>
2023-07-29 14:08:58 -0300

    - Inicio nas validações no componente editar

--- src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { PensamentoService } from '../pensamento.service';
 import { Pensamento } from './../pensamento/pensamento';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-pensamento',
@@ -18,12 +19,15 @@ export class EditarPensamentoComponent implements OnInit {
     conteudo: '',
     autoria: '',
     modelo: ''
-  }
+  }  
+
+  public formulario!: FormGroup;
 
   constructor(
     pensamentoService: PensamentoService,
     router: Router,
-    route: ActivatedRoute
+    route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) { 
     this._pensamentoService = pensamentoService;
     this._router = router;
@@ -31,13 +35,32 @@ export class EditarPensamentoComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+    this.formulario = this.formBuilder.group({
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      modelo: ['modelo1', [Validators.required]],
+    });
+
+    
     const id = parseInt(this._route.snapshot.paramMap.get('id')!);
-    this._pensamentoService.obterPorId(id).subscribe((pensamento) => 
-    this.pensamento = pensamento)    
+    this._pensamentoService.obterPorId(id).subscribe((pensamento) => {
+    this.formulario.value.conteudo = pensamento.conteudo,
+    this.formulario.value.autoria = pensamento.autoria,
+    this.formulario.value.modelo = pensamento.modelo
+  })    
   }
 
   public salvarPensamento(): void {
-    this._pensamentoService.editar(this.pensamento).subscribe();
+    this._pensamentoService.editar(this.formulario.value).subscribe();
     alert('Atualizado com sucesso!');
     this._router.navigate(['/listarPensamentos']);
   }
